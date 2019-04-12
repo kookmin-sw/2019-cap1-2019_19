a400bf206caaa3c92cbff53b9e0eaeb7884c88b2
eomjeongho <eomjeongho1@naver.com>
2019-04-12 18:23:10 +0900

    DB 변경으로 인한 수정

--- src/web/login/login.php
+++ src/web/login/login.php
@@ -1,37 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login Check</title>
-	<meta content="text/html"/>
-</head>
-<body>
 <?php
-	include "./db.php";
+    // db 연결
+    include "./connect.php";
 
-	$id = $_POST['id'];
-	$pw = $_POST['pw'];
+    // session 생성
+    session_start();
 
-	$filter = ['id' => $id, 'pw' => $pw];
-	$options = [
-		'projection' => ['_id' => 0]
-	];
+    // 입력받은 id, pw
+    $id = $_POST['id'];
+    $pw = $_POST['pw'];
 
-	$query = new MongoDB\Driver\Query($filter, $options);
-	$rows = $mongo->executeQuery("Login.users", $query);
-	$login = current($rows->toArray());
+    $query="SELECT * FROM Login WHERE id='$id' and pw='$pw'";
+    $result = mysqli_query($conn,$query);
+    $row = mysqli_fetch_array($result);
 
-	if(!empty($login)){
-		if(($id == $login->id) && ($pw == $login->pw)){
-			if($id == "admin"){
-				echo "Welcome Admin!<br><br>";
-			}
-                        session_start();
-			$_SESSION["is_logged"] = "Y";
-			echo("<script>location.replace('../main/main.php');</script>");
-		}
-	}else{
-		echo("<script>location.replace('./re_login.html');</script>");
-	}
+    if($id == $row['id'] && $pw == $row['pw']){
+        $_SESSION["is_logged"] = "Y";
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["ip"] = $row["ip"];
+        echo("<script>location.replace('../main/main.php');</script>");
+    }else{
+        echo("<script>location.replace('./re_login.php');</script>");
+    }
 ?>
-</body>
-</html>
