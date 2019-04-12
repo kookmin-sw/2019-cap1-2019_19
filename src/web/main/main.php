28f696a2ce7df15e969b5bc99aee0380b14d291e
eomjeongho <eomjeongho1@naver.com>
2019-04-12 18:36:43 +0900

    tab 간격 조절

--- src/web/main/main.php
+++ src/web/main/main.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-  <?php
-    session_start();
-    $check = $_SESSION['is_logged'];
-    if($check != "Y"){
-      echo("<script>location.replace('../index.html');</script>"); 
-    }
-  ?>
-  <head>
+<?php
+session_start();
+$check = $_SESSION['is_logged'];
+if($check != "Y"){
+  echo("<script>location.replace('../index.html');</script>"); 
+}
+?>
+<head>
     <meta charset = "UTF-8">
     <title>Main Page</title>
     <style type="text/css">
@@ -18,8 +18,8 @@
         width: 100%;
         min-height: 300px;
         min-width: 300px;
-      }
-      .header{
+    }
+    .header{
         display: fixed;
         top:0; left:0; right:0;
         width: 100%;
@@ -29,23 +29,23 @@
         text-align: center;
         color: #FF007F;
         z-index: 4;
-      }                  
-      .content{
+    }                  
+    .content{
         position: relative;
         top:-70; left:0; right:0;
         width: 100%;
         height: 90%;
         text-align: center;
         z-index: 1;
-      }
-      .opt{
+    }
+    .opt{
         position: relative;
         width: 50%;
         height:100%;
         text-align: center;
         font-size: 100px;
-      }
-      .footer{
+    }
+    .footer{
         margin:0px;
         position: fixed;
         bottom: 0; left:0; right:0;
@@ -55,23 +55,23 @@
         font-size: 20px;
         text-align: right;
         z-index: 3;
-      }
-    </style>
-  </head>
-  <body>
+    }
+</style>
+</head>
+<body>
     <div class="header">
       Smart Interphone
-    </div>
-    <div class="content">
+  </div>
+  <div class="content">
       <div class="opt" style="float: left; background-color: #E4F7BA;" onclick="location.href='./streaming.php'">
         <br><br><br>Streaming
-      </div>
-      <div class="opt" style="float: right; background-color: #CEFBC9;" onclick="location.href='./history.php'">
+    </div>
+    <div class="opt" style="float: right; background-color: #CEFBC9;" onclick="location.href='./history.php'">
         <br><br><br>History
-      </div>
     </div>
-    <div class="footer">
-      2019 캡스톤 디자인 I - 19조(5정호)
-    </div>
-  </body>
+</div>
+<div class="footer">
+  2019 캡스톤 디자인 I - 19조(5정호)
+</div>
+</body>
 </html>
