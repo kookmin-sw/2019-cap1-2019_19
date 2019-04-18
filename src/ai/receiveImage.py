2ff7caf04d3d11cfd1bf70cbc79adbae6ffd3efd
20163140 <dltnwls8751@gmail.com>
2019-04-18 21:30:31 +0900

    사진 저장 & 서브프로세스 코드 추가

--- src/ai/receiveImage.py
+++ src/ai/receiveImage.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import socket
 import sys
 import time
+import base64
+import subprocess
 
 HOST = '' #all available interfaces
-PORT = ####
+PORT = 2222
 
 #1. open Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,30 +32,37 @@ while 1:
     #4. Accept connection
     conn, addr = s.accept()
     print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-
-
+    
+    
     for i in range(1, 4):
         #5. Read/Send
         img = ""
         len_data = conn.recv(1024).decode()
         len_data = int(len_data)
         size = 0
-
-
-        print("type size", type(size))
+        
+        
+        #        print("type size", type(size))
+        print("Data Reading")
         while 1:
-            print("데이터 읽는중")
+            
             data = conn.recv(65536)
             str_data = data.decode()
             img += data.decode()
+            #f.write(base64.b64decode(data))
             size += len(str_data)
-            print(size, type(size), len_data, type(len_data))
+            #            print(size, type(size), len_data, type(len_data))
             if size == len_data:
-                print("데이터 읽기끝")
+                print("Finished Data Reading")
                 break
-        #print(img)
-        print(i)
+    #print(img)
+    #        print(i)
+
+    with open('/root/openface/temp/test_'+str(i)+'.jpeg', 'wb') as f:
+        f.write(base64.b64decode(img))
         time.sleep(0.1)
-    break 
+for j in range(1,4):
+    subprocess.call(['/root/openface/demos/classifier_test.py infer /root/openface/embedding/us/classifier.pkl /root/openface/temp/test_'+str(j)+'.jpeg'], shell=True)
+#    break
 conn.close()
 s.close()
