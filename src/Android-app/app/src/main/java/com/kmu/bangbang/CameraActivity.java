8ba708ca4a753632791ff2b29b6ef00b9ea5b178
Hyean <hean1103@naver.com>
2019-05-03 23:55:12 +0900

    카메라 프리뷰 수정 및 External Storage 기능 추가

--- src/Android-app/app/src/main/java/com/kmu/bangbang/CameraActivity.java
+++ src/Android-app/app/src/main/java/com/kmu/bangbang/CameraActivity.java
@@ -1,60 +1,92 @@
 package com.kmu.bangbang;
-import java.io.IOException;
-import android.app.Activity;
-import android.graphics.PixelFormat;
-import android.hardware.Camera;
+
+import android.media.MediaRecorder;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+//import android.view.Menu;
+//import android.view.MenuItem;
+
+import java.io.IOException;
 
 
-public class CameraActivity extends Activity implements SurfaceHolder.Callback{
+public class CameraActivity extends AppCompatActivity{
 
-    Camera camera;
     SurfaceView surfaceView;
-    SurfaceHolder surfaceHolder;
-    boolean previewing = false;
+    SurfaceHolder holder;
+    MediaRecorder recorder;
 
-    String stringPath ="/sdcard/samplevideo.3gp";
+
+    String path ="/sdcard/recorded_video.mp4";
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        getWindow().setFormat(PixelFormat.UNKNOWN);
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceview);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
 
-      if(!previewing){
-          camera = Camera.open();
-          if(camera !=null){
-              try{
-                  camera.setPreviewDisplay(surfaceHolder);
-                  camera.startPreview();
-                  previewing=true;
-              }catch (IOException e){
-                  e.printStackTrace();
-              }
-          }
-      }
+        holder = surfaceView.getHolder();
 
 
     }
+    public void onButtonClicked(View v){
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+        try {
+            if (recorder != null) {
+                recorder.stop();
+                recorder.release();
+                recorder = null;
+            }
+
+
+            recorder = new MediaRecorder();
+
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+
+            recorder.setOutputFile(path);
+            recorder.setPreviewDisplay(holder.getSurface());
+            recorder.prepare();
+            recorder.start();
+
+            Toast.makeText(getApplicationContext(),"녹화를 시작합니다.",Toast.LENGTH_LONG).show();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void onButton2Clicked(View v){
+        if(recorder!=null){
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+        }
+
+        Toast.makeText(getApplicationContext(),"녹화를 중지합니다.",Toast.LENGTH_LONG).show();
 
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder){
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.menu_main,menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        int id = item.getItemId();
+//        if(id==R.id.action_settings){
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder){
-
-    }
 }
+
