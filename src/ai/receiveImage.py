e916a74b502886becf0927bf7cb6863a5a648433
20163140 <dltnwls8751@gmail.com>
2019-05-05 11:22:47 +0900

    얼굴 인식 결과 처리-외부인/사용자 or 지인

--- src/ai/receiveImage.py
+++ src/ai/receiveImage.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
 import socket
 import sys
@@ -7,7 +7,7 @@ import base64
 import subprocess
 
 HOST = '' #all available interfaces
-PORT = 2222
+PORT = 2222 
 
 #1. open Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,44 +26,62 @@ print ('Socket bind complete')
 s.listen(10)
 print ('Socket now listening')
 
-
 #keep talking with the client
 while 1:
     #4. Accept connection
     conn, addr = s.accept()
     print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-    
-    
+
     for i in range(1, 4):
         #5. Read/Send
         img = ""
         len_data = conn.recv(1024).decode()
         len_data = int(len_data)
         size = 0
-        
-        
-        #        print("type size", type(size))
-        print("Data Reading")
+
+        print("Data Reading") 
+
         while 1:
-            
             data = conn.recv(65536)
             str_data = data.decode()
             img += data.decode()
-            #f.write(base64.b64decode(data))
             size += len(str_data)
-            #            print(size, type(size), len_data, type(len_data))
             if size == len_data:
                 print("Finished Data Reading")
                 break
-    #print(img)
-    #        print(i)
 
         with open('/root/openface/temp/test_'+str(i)+'.jpeg', 'wb') as f:
             f.write(base64.b64decode(img))
-            time.sleep(0.1)
-    for j in range(1,4):
-        res = subprocess.check_output(['/root/openface/demos/classifier_test.py infer /root/openface/embedding/us/classifier.pkl /root/openface/temp/test_'+str(j)+'.jpeg'], universal_newlines=True,shell=True)
-        print("res: {}".format(res))
-#    break
+        time.sleep(0.1)
+
+    break
+
+res_list = {}
+
+for j in range(1,4):
+    # start face recognigionc
+    res=subprocess.check_output(['/root/openface/demos/classifier_test.py infer /root/openface/embedding/us/classifier.pkl /root/openface/temp/test_'+str(j)+'.jpeg'], universal_newlines=True,shell=True)
+    print("res: {}".format(res))
+    
+    # store recognition result
+    name, accuracy = res.split(':')
+    accuracy = float(accuracy)
+
+    if accuracy > 0.5:
+        if name in res_list.keys():
+            res_list[name][1] += 1
+            if res_list[name][0] < accuracy:
+            	res_list[name][0] = accuracy
+        else:
+            res_list[name] = [accuracy, 1]
+
+    print("res_list: {}".format(res_list))
+
+if not res_list:
+    print("stranger")
+else:
+    res_list = sorted(res_list.items(), key = lambda x: (x[1][1], x[1][0]), reverse = True)
+    print("final res: {}".format(res_list[0][0]))
+
 conn.close()
 s.close()
