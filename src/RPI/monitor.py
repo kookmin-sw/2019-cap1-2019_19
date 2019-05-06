3d9d699588d348b7e11fa662cb260b2c404df96d
eomjeongho <eomjeongho1@naver.com>
2019-05-06 15:50:07 +0900

    모니터 스트리밍 구현

--- src/RPI/monitor.py
+++ src/RPI/monitor.py
@@ -1,16 +1,60 @@
+#-*- coding: utf-8 -*-
 from tkinter import *
-from PIL import Image
+from PIL import Image, ImageTk
+import cv2
+import pymysql
+from subprocess import check_output
+
+def myIP():
+	# local ip
+	ip = check_output(['hostname', '-I']).decode('utf8')
+	ip = ip.replace('\n','')
+	ip = ip.replace(' ','')
+	return ip
+
+def getIP():
+	# DB에서 스트리밍 서버 IP 가져오기
+	conn = pymysql.connect(host='52.78.219.61', port=3306, user='monitor', passwd="Kookmin1!", db='db', charset='utf8')
+	myip = myIP()
+
+	curs = conn.cursor()
+
+	sql = "select * from Login where m_ip=%s"
+	curs.execute(sql,myip)
+
+	camip = "0.0.0.0"
+	rows = curs.fetchall()
+	if len(rows) != 1:
+		print("등록된 기기가 없습니다")
+	else:
+		camip = rows[0][2]
+	
+	conn.close()
+	return camip
 
 win = Tk()
 win.title("Smart Interphone")
 win.geometry('800x480+0+0')
 win.resizable(0,0)
 
-video = Frame(win, width = 640, height = 480, bg = "black")
+video = Frame(win, width = 640, height = 480)
+video.grid()
+screen = Label(video)
+screen.grid()
+ip = getIP()
+camera = cv2.VideoCapture('http://'+ip+':8080/stream/video.mjpeg')
 video.pack(side="left")
-
 menu = Frame(win, width = 160, height = 480)
 menu.pack()
+
+def stream():
+	_, frame = camera.read()
+	cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+	img = Image.fromarray(cv2image)
+	imgtk = ImageTk.PhotoImage(image=img)
+	screen.imgtk = imgtk
+	screen.configure(image=imgtk)
+	screen.after(1, stream)
 
 # 0: Off, 1: On
 global mic_state
@@ -39,6 +83,8 @@ def mic_off():
 
 	print("mic_off")
 	print("mic_state: ", mic_state)
+
+stream()
 
 mic_on = Button(menu, text="MIC ON", width = 160, height=15, bg = "green", command= mic_on)
 mic_on.pack(expand=1)
