122143f06ef709046401e3ca13276b03c1cea574
SeungAe <ghkdtmddo01@naver.com>
2019-05-08 18:34:05 +0900

    동영상 저장시 영상 가져오는 방식 변경(카메라 -> 스트리밍 서버)

--- src/RPI/sendVideo.py
+++ src/RPI/sendVideo.py
@@ -2,47 +2,65 @@
 
 import RPi.GPIO as GPIO
 import time
-from picamera import PiCamera
 import datetime
 import requests
+import cv2
 
 # PIR 센서 설정
 GPIO.setmode(GPIO.BCM)
 pirPin = 18
 GPIO.setup(pirPin, GPIO.IN, GPIO.PUD_UP)
 
-# Camera 설정
-camera = PiCamera()
+capture_duration = 41
 
 # 파일 이름을 위한 date
 date = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
 
-while True:
+# 스트리밍 서버에서 영상 읽기
+camera = cv2.VideoCapture("http://localhost:8080/stream/video.mjpeg")
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter(date+'.mp4', fourcc, 20, (640,480))
+
+# 카메라가 열렸는지 확인
+if(camera.isOpened() == False):
+    print("Error opening video stream or file")
+
+
+# 카메라가 제대로 열렸으면
+while(camera.isOpened()):
     # PIR 센서가 사람 인식
     if GPIO.input(pirPin) == GPIO.HIGH:
         print("사람 감지")
-        camera.start_preview()
-        # 영상 촬영 및 저장할 장소
-        print("촬영 시작")
-        camera.start_recording('/home/pi/Code/%s.h264' % date)
-        # 사람이 감지되고 30초 동안 영상 촬영
-        time.sleep(30)
-        # 영상 촬영 종료
-        camera.stop_recording()
-        print("촬영 종료")
-        camera.stop_preview()
+        start_time = time.time()
+        while(int(time.time() - start_time) < capture_duration) :
+            #영상을 한 프레임씩 읽어오기
+            #(영상을 제대로 읽으면 ret=true, 잘못 읽으면 ret=false)
+            ret, frame = camera.read()
 
-        time.sleep(1)
+            #영상을 제대로 읽었을 때
+            if ret:
+                # 이미지 반전 0:상하, 1:좌
+                frame = cv2.flip(frame, 0)
+                out.write(frame)
+                cv2.imshow('frame', frame)
+
+            else:
+                break
+
+        time.sleep(3)
 
         # 영상 웹서버에 전송
         print("웹서버에 전송 시작")
         # 전송할 웹 서버 주소
         upload_url = 'http://52.78.219.61/video_upload.php'
         # 전송할 파일 설정
-        file_ = {'myfile': (date+'.h264', open(date+'.h264', 'rb'))}
+        file_ = {'myfile': (date+'.mp4', open(date+'.mp4', 'rb'))}
         # 파일 전송
         r = requests.post(upload_url, files=file_)
-        print (r.text)
+        print(r.text)
 
-camera.close()
+        time.sleep(30)
 
+camera.release()
+out.release()
+cv2.destroyAllWindows()
