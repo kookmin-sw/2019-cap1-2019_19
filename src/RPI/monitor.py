40f6544d299aa0b49601c2133ed21f8b8e098433
eomjeongho <eomjeongho1@naver.com>
2019-05-09 09:17:07 +0900

    로그인 수정 및 가상 키보드 추가

--- src/RPI/monitor.py
+++ src/RPI/monitor.py
@@ -12,7 +12,6 @@ camIP = ""
 # 창 닫기
 def close(window):
 	window.destroy()
-
 # 로그인
 def loginCheck(login, id, pw):
 	global user_id
@@ -23,7 +22,7 @@ def loginCheck(login, id, pw):
 	user_pw = pw.get()
 
 
-	conn = pymysql.connect(host='52.78.219.61', port=3306, user='monitor', passwd="Kookmin1!", db='db', charset='utf8')
+	conn = pymysql.connect(host='####', port=0000, user='####', passwd="####", db='####', charset='utf8')
 	curs = conn.cursor()
 
 	sql = "select ip from Login where id = %s and pw = %s"
@@ -33,10 +32,109 @@ def loginCheck(login, id, pw):
 	# 등록된 경우 camIP 저장 후 로그인 창 닫기
 	if len(rows) == 1:
 		camIP = rows[0][0]
-		close(Login)
+		close(login)
+
+# Key 눌렸을 때 처리
+def select(entry,value,upper):
+	if value == "BACK":
+		entry.delete(len(entry.get())-1,END)
+	elif value == "CLEAR":
+		entry.delete(0,END)
+	else:
+		# 대문자
+		if upper == 1:
+			value = value.upper()
+		entry.insert(END, value)
+
+# Keyboard 생성
+def makeKeyboard(window, entry, buttons, upper):
+	baseX = 0
+	baseY = 230
+	w = 64
+	h = 50
+	count = 0
+
+	for button in buttons:
+		command = lambda x=button: select(entry,x,upper)
+		if upper == 1:
+			button = button.upper()
+		if button == "CLEAR":
+			Button(window,text= button,width=12, bg="#FFE08C", fg="#ffffff",
+				activebackground = "#ffffff", activeforeground="#FFE08C", relief='raised', font=("Arial",12,"bold"),
+				bd=1,command=command).place(x = baseX+count*w, y = baseY, width = w*3, height = h)
+		elif button == "SHIFT":
+			Button(window,text= button,width=4, bg="#FFE08C", fg="#ffffff",
+				activebackground = "#ffffff", activeforeground="#FFE08C", relief='raised', font=("Arial",12,"bold"),
+				bd=1,command=partial(makeKeyboard,window, entry, buttons, (upper+1)%2)).place(x = baseX+count*w, y = baseY, width = w, height = h)
+		elif button == "BACK":
+ 			Button(window,text= button,width=12, bg="#FFE08C", fg="#ffffff",
+				activebackground = "#ffffff", activeforeground="#FFE08C", relief='raised', font=("Arial",12,"bold"),
+				bd=1,command=command).place(x = baseX+count*w, y = baseY, width = w, height = h)
+		else:
+			Button(window,text= button,width=4, bg="#FFB2F5", fg="#ffffff",
+				activebackground = "#ffffff", activeforeground="#FFB2F5", relief='raised', font=("Arial",12,"bold"),
+				bd=1,command=command).place(x = baseX+count*w, y = baseY, width = w, height = h)
+
+		count +=1
+
+		if count > 9 and baseY == 230:
+			count = 0
+			baseY += h
+		if count > 9 and baseY == 280:
+			count = 0
+			baseY += h
+		if count > 9 and baseY == 330:
+			count = 0
+			baseY += h
+		if count > 9 and baseY == 380:
+			count = 0
+			baseY += h
+
+# 로그인 창
+def LoginWindow(window):
+	# Login 창 생성 및 설정
+	# size: 800x480, size 변경 불가, fullscreen
+	Login = Toplevel(window)
+	Login.title("Login")
+	Login.geometry("800x480+0+0")
+	Login.attributes('-fullscreen', True)
+	Login.resizable(False, False)
+	Login.lift()
+	Login.bind("<Escape>",lambda e: window.destroy())
+
+	# Login Frame
+	login = Frame(Login, width = 640, height = 480, bg="white")
+	login.pack(side="left")
+
+	# ID, PW 입력Entry
+	Label(login, text="ID", bg="white").place(x=0, y=75, width = 50, height = 25)
+	Label(login, text="PW", bg="white").place(x=0, y=125, width = 50, height = 25)
+	id = Entry(login)
+	pw = Entry(login)
+	id.place(x=50, y = 75, width = 540, height = 25)
+	pw.place(x=50, y = 125, width = 540, height = 25)
+
+	# onScreenKeyboard
+	buttons=[
+	'~','!','@','#','$','%','^','&','*','BACK',
+	'1','2','3','4','5','6','7','8','9','0',
+	'q','w','e','r','t','y','u','i','o','p',
+	'a','s','d','f','g','h','j','k','l','SHIFT',
+	'z','x','c','v','b','n','m','CLEAR']
+
+	makeKeyboard(login, id, buttons,0)
+	id.bind("<Button-1>",lambda e: makeKeyboard(login, id, buttons,0))
+	pw.bind("<Button-1>",lambda e: makeKeyboard(login, pw, buttons,0))
+
+	# Login 창 메뉴Frame
+	menu = Frame(Login, width = 160, height = 480)
+	menu.pack()
+
+	# Login 창 버튼: 로그인, 종료
+	bt_login = Button(menu, text="Login", bg="green", fg="#ffffff", activebackground = "#ffffff", activeforeground="green", font=("Arial",15,"bold"), command=partial(loginCheck, Login, id, pw)).place(x=0, y=0, width = 160, height = 480)
 
 # 스트리밍 창
-def Streaming(window):
+def StreamingWindow(window):
 	global camIP
 
 	# 스트리밍
@@ -52,7 +150,7 @@ def Streaming(window):
 	streaming=Toplevel(window)
 	streaming.title("streaming")
 	streaming.geometry("800x480+0+0")
-	#streaming.attributes('-fullscreen', True)
+	streaming.attributes('-fullscreen', True)
 	streaming.resizable(False,False)
 
 	# screen, camera 설정
@@ -77,7 +175,7 @@ def Streaming(window):
 	stream()
 
 # 방문기록 창
-def History(window):
+def HistoryWindow(window):
 	window.destroy()
 
 # main 창 생성 및 설정
@@ -85,51 +183,23 @@ def History(window):
 main=Tk()
 main.title("main")
 main.geometry("800x480+0+0")
-#main.attributes('-fullscreen', True)
+main.attributes('-fullscreen', True)
 main.resizable(False, False)
 main.lower()
-
-# Login 창 생성 및 설정
-# size: 800x480, size 변경 불가, fullscreen
-Login = Toplevel(main)
-Login.title("Login")
-Login.geometry("800x480+0+0")
-#Login.attributes('-fullscreen', True)
-Login.resizable(False, False)
-Login.lift()
 
 # main 창 Frame
 mainframe = Frame(main, width = 800, height = 480)
 mainframe.pack()
 
 # main 창 버튼: 스트리밍, 방문기록, 종료
-bt_streaming = Button(mainframe, text="Streaming", width = 800, height = 11, command=partial(Streaming, main))
+bt_streaming = Button(mainframe, text="Streaming", width = 800, height = 11, command=partial(StreamingWindow, main))
 bt_streaming.pack(expand=1)
-bt_history = Button(mainframe, text="History", width = 800, height = 11, command=partial(History, main))
+bt_history = Button(mainframe, text="History", width = 800, height = 11, command=partial(HistoryWindow, main))
 bt_history.pack(expan=1)
 bt_close = Button(mainframe, text="close", width = 800, height=11,bg="red", command= partial(close,main))
 bt_close.pack(expand=1)
 
-# Login Frame
-login = Frame(Login, width = 640, height = 480, bg="black")
-login.pack(side="left")
-
-# ID, PW 입력Entry
-Label(login, text="ID").grid(row=0)
-Label(login, text="PW").grid(row=1)
-id = Entry(login)
-pw = Entry(login)
-id.grid(row=0, column=1)
-pw.grid(row=1, column=1)
-
-# Login 창 메뉴Frame
-menu = Frame(Login, width = 160, height = 480)
-menu.pack()
-
-# Login 창 버튼: 로그인, 종료
-bt_login = Button(menu, text="Login", width = 160, height=17, bg="green", command=partial(loginCheck, Login, id, pw))
-bt_login.pack(expand=1)
-bt_login_close = Button(menu, text="Close", width = 160, height=16, bg="red", command=partial(close, main))
-bt_login_close.pack(expand=1)
+# 로그인 창 생성
+LoginWindow(main)
 
 main.mainloop()
