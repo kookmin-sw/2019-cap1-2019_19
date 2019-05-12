dfd790fc9c0d79c1f154846d8b7845837d64f14e
20163140 <dltnwls8751@gmail.com>
2019-05-13 01:53:14 +0900

    방문자 수 확인 기능 구현

--- src/Android-app/app/src/main/java/com/kmu/bangbang/HomeFragment.java
+++ src/Android-app/app/src/main/java/com/kmu/bangbang/HomeFragment.java
@@ -1,20 +1,143 @@
 package com.kmu.bangbang;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class HomeFragment extends Fragment {
+
+    private static String TAG = "HomeFragment";
+    String mJsonString;
+    TextView countText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, null) ;
+        countText = (TextView) (TextView)view.findViewById(R.id.countText);
+
+        GetData task = new GetData();
+        task.execute("http://52.78.219.61/VisitorCount.php");
+
+        return view;
+    }
+
+    private class GetData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(getActivity(),
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            // mTextViewResult.setText(result);
+            Log.d(TAG, "response  - " + result);
+            System.out.print("result ="+result);
+
+            // SQL문 오류 검사
+            if (result == null){
+                Toast.makeText(getActivity(), errorString, Toast.LENGTH_SHORT).show();
+            }
+            else {
+
+                mJsonString = result;
+                // 데이터 유무 검사
+                if(!mJsonString.equals("")){
+                    showResult();
+                }
+                else{
+                    Toast.makeText(getActivity(), "방문기록이 없습니다!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+        }
+    }
+
+    private void showResult(){
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+
+            int count = jsonObject.getInt("count");
+            countText.setText("오늘은 " + Integer.toString(count)+"명이\n 방문했습니다!");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/web/visitRecord/VisitorCount.php
@@ -0,0 +1,19 @@
+<?php
+    $result = array();
+    $link=mysqli_connect("localhost","admin","Kookmin1!", "db" );
+    if (!$link)
+    {
+        echo "MySQL 접속 에러 : ";
+        echo mysqli_connect_error();
+        exit();
+    }
+    mysqli_set_charset($link,"utf8");
+    $today = date("Y-m-d");
+    $sql="SELECT COUNT(*) FROM Historry WHERE DATE_FORMAT(rDate, '%Y-%m-%d') = '${today}'";
+    $count=mysqli_query($link,$sql);
+    $row=mysqli_fetch_array($count);
+    $row_result=$row[0];
+    $result["count"]=$row_result;
+    echo json_encode($result);
+    mysqli_close($link);
+?>
