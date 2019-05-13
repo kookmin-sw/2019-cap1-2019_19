30967480c9a9b99ee13a389138e181a9e8786f3f
Hyean <hean1103@naver.com>
2019-05-13 15:38:10 +0900

    비디오 업로드 php파일 추가

---
+++ src/web/upload.php
@@ -0,0 +1,19 @@
+<form enctype="multipart/form-data" method="post">
+<input type="hidden" name="MAX_FILE_SIZE" value="3000000"/>
+
+<?php
+    
+    $file_name = $_FILES['myFile']['name'];
+    $file_size = $_FILES['myFile']['size'];
+    $file_type = $_FILES['myFile']['type'];
+    $temp_name = $_FILES['myFile']['tmp_name'];
+    $location = "./uploads/";
+    
+    if(move_uploaded_file($_FILES['myFile']['tmp_name'],"$location/$file_name")){
+        echo $_FILES['myFile']['name'];
+    }else {
+        echo $_FILES['myFile']['size'];
+    }
+    
+    echo "error :";
+    print_r($_FILES);
