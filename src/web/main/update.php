83f77a7eabadb1e76a8ab6f26632a1e870d0706a
SeungAe <ghkdtmddo01@naver.com>
2019-05-23 00:08:54 +0900

    방문기록 수정 페이지 전반적으로 수정

--- src/web/main/update.php
+++ src/web/main/update.php
@@ -3,7 +3,7 @@ session_start();
 if($_SESSION["is_logged"] != "Y")
 echo("<script>location.replace('../login/re_login.php');</script>");
 // db 연결
-$mysqli = new mysqli('localhost', 'admin', 'Kookmin1!', 'db');
+$mysqli = new mysqli('localhost', 'monitor', 'Kookmin1!', 'db');
 if($mysqli->connect_errno) {
   exit('Error Connecting db');
 }
@@ -13,8 +13,36 @@ $name = $_POST['name'];
 $belong = $_POST['belong'];
 $rIdx = $_POST['rIdx'];
 
-$result = $mysqli->query("UPDATE `SEUNGAE` SET `name` = '{$name}', `belong` = '{$belong}' WHERE `rIdx` = {$rIdx}");
+$result = $mysqli->query("UPDATE `History` SET `name` = '{$name}', `belong` = '{$belong}' WHERE `rIdx` = {$rIdx}");
 
-echo " ";
-echo "Update 완료";
 ?>
+
+<!DOCTYPE html>
+<html>
+  <style type="text/css">
+    body {
+      margin:auto;
+    }
+
+    #content {
+      position: absolute;
+      top: 65%;
+      left: 50%;
+      height: 300px;
+      width: 300px;
+      margin: -150px 0 0 -150px;
+      color: #FF007F;
+      text-align: center;
+    }
+  </style>
+  <body>
+    <div id="content">
+      <h2> Updatet 완료 </h2>
+
+      <input type="button" value="닫기" onclick="window.close();">
+    </div>
+  </body>
+  <script>
+  </script>
+
+</html>
