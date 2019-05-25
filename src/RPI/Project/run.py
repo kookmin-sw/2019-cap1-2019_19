bbfb9cd654b3c1ad8ac14207af106a2adf9cf159
eomjeongho <eomjeongho1@naver.com>
2019-05-26 01:09:26 +0900

    소켓 연결 방식 변경, 알림 방식 변경, 사진,영상 DB 관련 변경에 따른 인자 전달 변경

--- src/RPI/Project/run.py
+++ src/RPI/Project/run.py
@@ -3,43 +3,52 @@ from Module import ip
 from Module import init
 from Module import pir
 from Module import image
-from Mpdule import video
+from Module import video
 from Module import button
 from Module import communication as c
 
 from time import sleep
+import datetime
 import threading
 import socket
 
-MONITOR = 'off'
-HOST = ''
-PORT = 0000
+#########
+# STATE #
+#########
+MONITOR = 'OFF'
 
-ip = ip.getMyIP()
-id, m_ip = init.init(ip)
+########
+# MAIN #
+########
+ip, e_ip = ip.getMyIP()
+id, m_ip = init.init(ip, e_ip)
+
 if m_ip != None:
-	MONITOR = 'ON'
-	monitor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	monitor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	monitor.bind((HOST,PORT))
-	monitor.listen(1)
-	print("listening...")
-	conn, addr = monitor.accept()
-	print(addr,"접속")
-	ct = threading.Thread(target=c.communication, args=(conn,)).start()
+	MONITOR = "ON"
+
+# 통신
+c_t = threading.Thread(target=c.connect, args=(m_ip,)).start()
+
+# PIR센서, Button 초기화
 pir.init()
 button.init()
 
 while True:
 	# 벨 눌른 경우 -> 모니터에 알림
-	if button.click():
-		msg = "bell"
-		conn.send(msg.encode('utf-8'))
-		print("버튼 누름")
+	if button.click() and MONITOR == "ON":
+		c.notification()
+		button.play()
 
 	if pir.visit() == 1:
-		print("방문자 확인")
-		stopPir = threading.Thread(target=pir.stop).start()
-		imageThread = threading.Thread(target=image.image, args=(id,)).start()
-		videoThread = threading.Thread(target=video.video, args=(id,)).start()
+		# 방문 시간
+		date_stamp = datetime.datetime.now()
+		date = date_stamp.strftime('%y%m%d_%H%M%S')
 
+		print("방문자 확인")
+
+		# PIR센서 무시
+		stopPir = threading.Thread(target=pir.stop).start()
+		# 사진
+		imageThread = threading.Thread(target=image.image, args=(id, date_stamp,)).start()
+		# 영상
+		videoThread = threading.Thread(target=video.video, args=(id, date, date_stamp,)).start()
