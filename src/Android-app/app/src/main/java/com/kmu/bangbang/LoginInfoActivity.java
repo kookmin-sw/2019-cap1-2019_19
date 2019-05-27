08c63797150b79858277992a0415105422181ecb
eunjuC1215 <eunju1215@kookmin.ac.kr>
2019-05-28 01:21:58 +0900

    설정페이지 수정 & 로그인 정보 UI

---
+++ src/Android-app/app/src/main/java/com/kmu/bangbang/LoginInfoActivity.java
@@ -0,0 +1,13 @@
+package com.kmu.bangbang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_info);
+    }
+}
--- src/Android-app/app/src/main/java/com/kmu/bangbang/SettingFragment.java
+++ src/Android-app/app/src/main/java/com/kmu/bangbang/SettingFragment.java
@@ -13,28 +13,34 @@ import android.widget.ListView;
 
 
 public class SettingFragment extends Fragment {
-    static final String[] LIST_MENU = {"인터넷 연결 설정","프로필 설정하기"} ;
+    private ListView listView;
+    static final String[] settings = {"로그인 정보","기기 등록"} ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_setting, null) ;
 
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU) ;
-
-        ListView listview1 = (ListView) view.findViewById(R.id.connect) ;
-        listview1.setAdapter(adapter) ;
-
-        listview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-
-                Intent intent = new Intent(getActivity(),ConnectActivity.class);
-                startActivity(intent);
-            }
-        });
+        listView = (ListView) view.findViewById(R.id.listView_setting);
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, settings));
+        listView.setOnItemClickListener(onitemClickListener);
 
         return view;
     }
+
+
+    private AdapterView.OnItemClickListener onitemClickListener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if(position == 0){
+                Intent intent = new Intent(getActivity(), LoginInfoActivity.class);
+                startActivity(intent);
+            }
+            if(position == 1){
+                Intent intent = new Intent(getActivity(), ConnectActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
